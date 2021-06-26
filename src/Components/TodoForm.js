45026392b6b57c61ae9a7316acd7773079abd1ba
Pranaya Anargya <panargya@gmail.com>
2021-06-26 17:17:17 +1000

    finished todolist

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
 
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const inputRef = useRef(null)
 
@@ -27,16 +27,34 @@ function TodoForm(props) {
     }
     return (
         <form className="todo-form" onSubmit={handelSubmit}>
-            <input
-                type="text"
-                placeholder="Add a todo"
-                value={input}
-                name="text"
-                className="todo-input"
-                onChange={handleChange}
-                ref={inputRef}
-            />
-            <button className="todo-button"> Add todo</button>
+            {props.edit ? (
+                <>
+                    <input
+                        type="text"
+                        placeholder="Update your Item"
+                        value={input}
+                        name="text"
+                        className="todo-input edit"
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className="todo-button edit"> Update</button>
+                </>
+            ) :
+                (<>
+                    <input
+                        type="text"
+                        placeholder="Add a todo"
+                        value={input}
+                        name="text"
+                        className="todo-input"
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className="todo-button"> Add todo</button>
+                </>
+                )}
+
         </form>
     )
 }
